f96ac1a7787d4726ce467a40fb0feae779dc4714
abdout <osmanabdout@hotmail.com>
2025-02-10 18:00:14 +0300

    x

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,9 +1,12 @@
 import { PrismaClient } from "@prisma/client";
 
 declare global {
-  var prisma: PrismaClient | undefined;
+  let prisma: PrismaClient | undefined;
 }
 
-export const db = globalThis.prisma || new PrismaClient();
+// Explicitly type global to allow prisma property
+const globalForPrisma = global as { prisma?: PrismaClient }
 
-if (process.env.NODE_ENV !== "production") globalThis.prisma = db;
+export const db = globalForPrisma.prisma || new PrismaClient();
+
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
