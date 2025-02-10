a11c41a5c66fd800008b0cbb5363e438a2e96107
abdout <osmanabdout@hotmail.com>
2025-02-10 22:09:28 +0300

    x

--- src/components/template/timeline/example.tsx
+++ src/components/template/timeline/example.tsx
@@ -10,11 +10,11 @@ export function TimelineDemo() {
       title: "2024",
       content: (
         <div dir="rtl">
-          {/* <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
           المشاركة في إجماع قوى الحراك الوطني والتواصل مع الفاعلين في المجتمع.
           <br />
-          تفعيل ورقة "الاقتصاد التشاركي" ببدأ نشاط اقتصادي تشاركي لدعمها بمثال من الواقع العملي.
-          </p> */}
+          تفعيل ورقة &quot;الاقتصاد التشاركي&quot; ببدأ نشاط اقتصادي تشاركي لدعمها بمثال من الواقع العملي.
+          </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
 
@@ -58,15 +58,15 @@ export function TimelineDemo() {
       title: "2023",
       content: (
         <div dir="rtl">
-          {/* <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-8">
           إنتاج أوراق وبرامج خاصة بالحركة لتطوير الرؤية والاستراتيجية.
           <br />
-          إقامة دار "الحركة الوطنية" بالعمارات كمقر للنشاط السياسي والفكري.
+          إقامة دار &quot;الحركة الوطنية&quot; بالعمارات كمقر للنشاط السياسي والفكري.
           <br />
           العمل الثقافي من خلال لقاءات، وإنتاج فيديوهات ومقالات توعوية.
           <br />
-          إطلاق مبادرة "حكماء السودان" للممارسة سياسية راشدة.
-          </p> */}
+          إطلاق مبادرة &quot;حكماء السودان&quot; للممارسة سياسية راشدة.
+          </p>
           
           <div className="grid grid-cols-2 gap-4">
             <Image
@@ -113,13 +113,13 @@ export function TimelineDemo() {
       title: "2022",
       content: (
         <div dir="rtl">
-          {/* <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
+          <p className="text-neutral-800 dark:text-neutral-200 text-xs md:text-sm font-normal mb-4">
           تأسيس &quot;الحركة الوطنية للبناء والتنمية&quot;
           <br />
           توسيع النشاط الطلابي والمجتمعي، والتحدث إلى قادة العمل المجتمعي.
           <br />
           إنتاج محتوى فكري لإحياء أصول الدين ومختلف قضايا الشأن السوداني.
-          </p> */}
+          </p>
           <div className="grid grid-cols-2 gap-4">
             <Image
               src="/timeline/13.jpg"
