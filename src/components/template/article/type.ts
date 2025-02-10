c06431507aa796becafeab9502a57480412a09dd
abdout <osmanabdout@hotmail.com>
2025-02-10 11:19:05 +0300

    x

--- src/components/template/article/type.ts
+++ src/components/template/article/type.ts
@@ -12,3 +12,26 @@ export type author = {
   label: string
   image?: string
 }
+
+export interface Article {
+  _id?: string;
+  title: string;
+  desc: string;
+  body: string;
+  image: string;
+  author: string;
+  slug: string;
+  catSlug: string;
+  updatedAt: string;
+}
+
+export interface ArticleContextProps {
+  article: Article | null;
+  articles: Article[];
+  fetchArticle: (slug: string) => void;
+  fetchArticles: () => void;
+  refreshArticles: () => void;
+  recentArticles: () => void;
+  deleteArticle: (id: string) => void;
+
+}
