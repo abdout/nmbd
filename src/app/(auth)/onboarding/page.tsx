0d677d7f8cd3c39d7e1c252ff8dcc1203dbc14ea
abdout <osmanabdout@hotmail.com>
2025-02-10 15:38:27 +0300

    x

--- src/app/(auth)/onboarding/page.tsx
+++
@@ -1,7 +0,0 @@
-import { onboardingRoutes } from '@/components/add/types';
-import { redirect } from 'next/navigation';
-import React from 'react';
-
-export default function AddPage() {
-  redirect(onboardingRoutes.TERMS);
-}
