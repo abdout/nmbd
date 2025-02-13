e4c1ad03654cc73fac7c4f701d1fe09404dfc556
abdout <osmanabdout@hotmail.com>
2025-02-13 22:47:48 +0300

    x

--- src/components/platform/task/create.tsx
+++ src/components/platform/task/create.tsx
@@ -239,7 +239,7 @@ const Create: React.FC<CreateProps> = ({ onClose }) => {
                             <FormField
                                 control={form.control}
                                 name="club"
-                                render={({ field: _ }) => (
+                                render={({ field}) => (
                                     <FormItem>
                                         <FormControl>
                                             <SelectPopover
