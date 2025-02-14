6014094e378829c69dab51fbddb834ca7ae70472
abdout <osmanabdout@hotmail.com>
2025-02-14 09:26:18 +0300

    x

--- src/app/api/member/[id]/route.ts
+++ src/app/api/member/[id]/route.ts
@@ -3,9 +3,26 @@ import Member from "@/components/platform/member/model";
 import { NextResponse } from "next/server";
 import { NextRequest } from "next/server";
 
-export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params;
-  const { newName: name, newDob: dob, newAddress: address, newGender: gender, newRank: rank, newInterest: interest, newSkill: skill, newClub: club, newImage: image, newContact: contact } = await request.json() as { 
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function PUT(request: NextRequest, context: Context) {
+  const { id } = context.params;
+  const { 
+    newName: name, 
+    newDob: dob, 
+    newAddress: address, 
+    newGender: gender, 
+    newRank: rank, 
+    newInterest: interest, 
+    newSkill: skill, 
+    newClub: club, 
+    newImage: image, 
+    newContact: contact 
+  } = await request.json() as { 
     newName: string, 
     newDob: string, 
     newAddress: string, 
@@ -15,15 +32,32 @@ export async function PUT(request: NextRequest, { params }: { params: { id: stri
     newSkill: string, 
     newClub: string,
     newImage: string,
-    newContact: { phone: string, facebook: string, whatsapp: string } 
+    newContact: { 
+      phone: string, 
+      facebook: string, 
+      whatsapp: string 
+    } 
   };
+
   await connectDB();
-  await Member.findByIdAndUpdate(id, { name, dob, address, gender, rank, interest, skill, club, image, contact });
+  await Member.findByIdAndUpdate(id, { 
+    name, 
+    dob, 
+    address, 
+    gender, 
+    rank, 
+    interest, 
+    skill, 
+    club, 
+    image, 
+    contact 
+  });
+
   return NextResponse.json({ message: "Member updated" }, { status: 200 });
 }
 
-export async function GET({ params }: { params: { id: string } }) {
-  const { id } = params;
+export async function GET(request: NextRequest, context: Context) {
+  const { id } = context.params;
   await connectDB();
   const member = await Member.findById(id);
   return NextResponse.json({ member }, { status: 200 });
