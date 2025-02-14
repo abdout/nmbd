25e61e60bdc68562e0e73646608744c57d06d231
abdout <osmanabdout@hotmail.com>
2025-02-14 09:10:52 +0300

    x

--- src/app/api/member/[id]/route.ts
+++ src/app/api/member/[id]/route.ts
@@ -1,10 +1,16 @@
+import { NextRequest } from "next/server";
+import { NextResponse } from "next/server";
 import connectDB from "@/lib/mongodb";
 import Member from "@/components/platform/member/model";
-import { NextResponse } from "next/server";
-import { NextRequest } from "next/server";
 
-export async function PUT(request: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params;
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function PUT(request: NextRequest, context: Context) {
+  const { id } = context.params;
   const { newName: name, newDob: dob, newAddress: address, newGender: gender, newRank: rank, newInterest: interest, newSkill: skill, newClub: club, newImage: image, newContact: contact } = await request.json() as { 
     newName: string, 
     newDob: string, 
@@ -22,8 +28,8 @@ export async function PUT(request: NextRequest, { params }: { params: { id: stri
   return NextResponse.json({ message: "Member updated" }, { status: 200 });
 }
 
-export async function GET(request: NextRequest, { params }: { params: { id: string } }) {
-  const { id } = params;
+export async function GET(request: NextRequest, context: Context) {
+  const { id } = context.params;
   await connectDB();
   const member = await Member.findById(id);
   return NextResponse.json({ member }, { status: 200 });
