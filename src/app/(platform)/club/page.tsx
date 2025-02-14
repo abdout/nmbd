9e5a1dc5cae066715559dda395a733ac91d73f8f
abdout <osmanabdout@hotmail.com>
2025-02-14 14:45:56 +0300

    x

--- src/app/(platform)/club/page.tsx
+++ src/app/(platform)/club/page.tsx
@@ -10,15 +10,15 @@ const Club = () => {
         <h2 className="font-heading text-3xl leading-[1.1] sm:text-2xl md:text-3xl">
           الامانات
         </h2>
-        <p className="  pb-7">
-        سلطان العشق في الله ارضيك وين
+        <p className="pb-7">
+          سلطان العشق في الله ارضيك وين
         </p>
 
         <div className="w-full">
           <div className="items-center justify-center grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-6 xl:grid-cols-5 gap-6">
             {club.map((club) => (
-              <Link href={`/club/${club.id}`}>
-                <ClubCard key={club.id} {...club} />
+              <Link key={club.id} href={`/club/${club.id}`}>
+                <ClubCard {...club} />
               </Link>
             ))}
           </div>
--- src/app/(platform)/layout.tsx
+++ src/app/(platform)/layout.tsx
@@ -15,7 +15,7 @@ export default function AppLayout({ children }: AppLayoutProps) {
   return (
     <div data-wrapper="" className="px-4 md:px-8 lg:px-16 xl:px-32 2xl:px-48 border-grid flex flex-1 flex-col">
       <PlatformHeader />
-      <main className="flex flex-1 flex-col">
+      <main className="flex flex-1 flex-col pt-8">
         <ProjectProvider>
           <TaskProvider>
             <UploadProvider>
