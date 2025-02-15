73d3f0e97c02202c0fcc48f6dcf56009f6299608
abdout <osmanabdout@hotmail.com>
2025-02-15 16:07:55 +0300

    x

--- src/app/(auth)/onboarding/attachment/actions.ts
+++ src/app/(auth)/onboarding/attachment/actions.ts
@@ -1,22 +1,22 @@
-'use server';
-import { stepOneSchema } from '@/components/add/schemas';
-import { AddDealRoutes, FormErrors } from '@/components/add/types';
-import { redirect } from 'next/navigation';
+// 'use server';
+// import { stepOneSchema } from '@/components/add/schemas';
+// import { AddDealRoutes, FormErrors } from '@/components/add/types';
+// import { redirect } from 'next/navigation';
 
-export const stepOneFormAction = (
-  prevState: FormErrors | undefined,
-  formData: FormData
-): FormErrors | undefined => {
-  const data = Object.fromEntries(formData.entries());
-  const validated = stepOneSchema.safeParse(data);
-  if (!validated.success) {
-    const errors = validated.error.issues.reduce((acc: FormErrors, issue) => {
-      const path = issue.path[0] as string;
-      acc[path] = issue.message;
-      return acc;
-    }, {});
-    return errors;
-  }
+// export const stepOneFormAction = (
+//   prevState: FormErrors | undefined,
+//   formData: FormData
+// ): FormErrors | undefined => {
+//   const data = Object.fromEntries(formData.entries());
+//   const validated = stepOneSchema.safeParse(data);
+//   if (!validated.success) {
+//     const errors = validated.error.issues.reduce((acc: FormErrors, issue) => {
+//       const path = issue.path[0] as string;
+//       acc[path] = issue.message;
+//       return acc;
+//     }, {});
+//     return errors;
+//   }
 
-  redirect(AddDealRoutes.COUPON_DETAILS);
-};
+//   redirect(AddDealRoutes.COUPON_DETAILS);
+// };
--- src/app/(auth)/onboarding/review/page.tsx
+++ src/app/(auth)/onboarding/review/page.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
-import ReviewForm from './ReviewForm';
+// import ReviewForm from './ReviewForm';
 
 export default function StepTwo() {
   return (
     <div>
-      <ReviewForm />
+      {/* <ReviewForm /> */}
+      hello
     </div>
   );
 }
