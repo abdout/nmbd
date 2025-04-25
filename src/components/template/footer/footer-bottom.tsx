bd98101b51495c33f3064f5690093dd9024344ce
abdout <osmanabdout@hotmail.com>
2025-04-25 23:26:38 +0300

    footer

--- src/components/template/footer/footer-bottom.tsx
+++ src/components/template/footer/footer-bottom.tsx
@@ -2,9 +2,9 @@ import { Card, CardContent } from "@/components/ui/card";
 
 export function FooterAddressComponent() {
     return (
-      <Card className="w-full bg-transparent dark:bg-transparent border-none shadow-none">
+      <Card className="w-full bg-transparent border-none shadow-none">
         <CardContent className="px-4">
-          <div className="flex space-x-8 text-sm items-center justify-center md:justify-start text-gray-500 dark:text-gray-400">
+          <div className="flex space-x-8 text-sm items-center justify-center md:justify-start text-gray-400">
             <p className="text-[13px] md:text-[15px]">السودان</p>
             <p className="text-[13px] md:text-[15px]">العربية</p>
             <p className="text-[13px] md:text-[15px]">احصاء</p>
