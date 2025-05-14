61cc4f64b6876d1a0bc0de232aec9ddae70a0a73
abdout <osmanabdout@hotmail.com>
2025-05-14 23:07:08 +0300

    b

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -12,7 +12,7 @@ export default {
       center: true,
       padding: {
         DEFAULT: "0.5rem",
-        sm: "0.5rem",
+        sm: "1rem",
         md: "2rem",
         lg: "3rem",
         xl: "4rem",
